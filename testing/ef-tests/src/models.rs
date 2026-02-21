dff8b94039e159ad92e8201f2ecbdfac42044490
jsign <jsign.uy@gmail.com>
2026-02-21 13:55:55 -0300

    fix

--- testing/ef-tests/src/models.rs
+++ testing/ef-tests/src/models.rs
@@ -90,6 +90,10 @@ pub struct Header {
     pub requests_hash: Option<B256>,
     /// Target blobs per block.
     pub target_blobs_per_block: Option<U256>,
+    /// Block access list hash (EIP-7928).
+    pub block_access_list_hash: Option<B256>,
+    /// Slot number (EIP-7843).
+    pub slot_number: Option<U256>,
 }
 
 impl From<Header> for SealedHeader {
@@ -116,8 +120,8 @@ impl From<Header> for SealedHeader {
             excess_blob_gas: value.excess_blob_gas.map(|v| v.to::<u64>()),
             parent_beacon_block_root: value.parent_beacon_block_root,
             requests_hash: value.requests_hash,
-            block_access_list_hash: None,
-            slot_number: None,
+            block_access_list_hash: value.block_access_list_hash,
+            slot_number: value.slot_number.map(|v| v.to::<u64>()),
         };
         Self::new(header, value.hash)
     }
